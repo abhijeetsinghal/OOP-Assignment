5a2af41ef93aa29b0f2b3ce9ac7d4c10052b0d99
Prakhar <abhii.1409@gmail.com>
2020-08-12 15:05:21 +0530

    Greatest of 3 numbers

---
+++ Assignment-1/prog1_greatest_of_3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int r,c,i,j;
+   cout<<"Enter no. of rows"<<endl;
+   cin>>r;
+   cout<<"Enter no. of columns"<<endl;
+   cin>>c;
+  for(i=1;i<=r;i++)
+  {
+      for(j=1;j<=c;j++)
+  {
+   cout<<" * "<<endl;
+  }}
+return 0;
+}
